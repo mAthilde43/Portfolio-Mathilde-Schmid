022f71f79b63da746af01ea682d270514737c395
Mathilde Schmid <mathilde.schmid@armtek.fr>
2025-02-24 15:24:59 +0100

    Code Source

--- src/utils/Themes.js
+++ src/utils/Themes.js
@@ -1,11 +1,11 @@
 export const darkTheme = {
-  bg: "#1C1C27",
+  bg: "#16162E",
   bgLight: "#1C1E27",
   primary: "#4c80e6",
   text_primary: "#F2F3F4",
   text_secondary: "#b1b2b3",
   card: "#171721",
-  card_light: "#191924",
+  card_light: "#16162E",
   button: "#4c80e6",
   white: "#FFFFFF",
   black: "#000000",
